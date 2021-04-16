5179c621bccad9e730dd79870077e08f607ebed1
ilyakhranovskiy <82034943+ilyakhranovskiy@users.noreply.github.com>
2021-04-16 20:03:08 +0300

    Update lab numpy#2.py

--- lab numpy#2.py
+++ lab numpy#2.py
@@ -16,3 +16,21 @@ for i in range(w):
     for j in range(h):
         horizontal_gradient2[j][i] = horizontal_gradient2[j][i]-j
 cv2.imwrite('horizontal_grad2.jpg', horizontal_gradient2)
+
+
+vertical_gradient1=np.zeros((255,255))
+w = vertical_gradient1.shape[1]
+h = vertical_gradient1.shape[0]
+for i in range(w):
+    for j in range(h):
+        vertical_gradient1[i][j] = vertical_gradient1[i][j]+ j
+cv2.imwrite('vertical_gradient1.jpg', vertical_gradient1)
+
+
+vertical_gradient2=np.full((255,255),255)
+w = vertical_gradient2.shape[1]
+h = vertical_gradient2.shape[0]
+for i in range(w):
+    for j in range(h):
+        vertical_gradient2[i][j] = vertical_gradient2[i][j]-j
+cv2.imwrite('vertical_gradient2.jpg', vertical_gradient2)
